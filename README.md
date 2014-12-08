88dab87c71d39ba5e4bc8081841f07522b744867
Chylli <chylli.email@gmail.com>
2014-12-08 15:58:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
+# NAME
+
+HTML::FormBuilder - Blah blah blah
+
+# SYNOPSIS
+
+    use HTML::FormBuilder;
+
+# DESCRIPTION
+
+HTML::FormBuilder is
+
+# AUTHOR
+
+Chylli <chylli@binary.com>
+
+# COPYRIGHT
+
+Copyright 2014- Chylli
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
+=======
 perl-HTML-FormBuilder
-=====================
+
---
+++ lib/HTML/FormBuilder.pm
@@ -0,0 +1,39 @@
+package HTML::FormBuilder;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+HTML::FormBuilder - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use HTML::FormBuilder;
+
+=head1 DESCRIPTION
+
+HTML::FormBuilder is
+
+=head1 AUTHOR
+
+Chylli E<lt>chylli@binary.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Chylli
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
