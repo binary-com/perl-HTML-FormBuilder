29e73c2226c209833c204cdf16b4322553d766a2
Fayland Lam <fayland@gmail.com>
2015-01-12 10:20:24 +0800

    0.03

--- lib/HTML/FormBuilder/Field.pm
+++ lib/HTML/FormBuilder/Field.pm
@@ -3,7 +3,7 @@ package HTML::FormBuilder::Field;
 use strict;
 use warnings;
 use 5.008_005;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Carp;
 use Scalar::Util qw(weaken blessed);
--- lib/HTML/FormBuilder/Select.pm
+++ lib/HTML/FormBuilder/Select.pm
@@ -2,7 +2,7 @@ package HTML::FormBuilder::Select;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Carp;
 use Moo;
